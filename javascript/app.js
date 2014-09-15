7202b1692de86aecd46d62c7de7f947096785155
timbits07 <tim.storey@yahoo.com>
2014-09-15 16:15:40 -0700

    angular routes finished

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,31 @@
-var app = angular.module("twrApp");
+var twrApp = angular.module('twrApp', ['ngRoute']);
+
+twrApp.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'homeCtrl'
+    	})
+      .when('/about', {
+        templateUrl: 'partials/about.html',
+        controller: 'aboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'partials/contact.html',
+        controller: 'contactCtrl'
+    	})
+      .when('/media', {
+        templateUrl: 'partials/media.html',
+        controller: 'mediaCtrl'
+      })
+      .when('/tour', {
+        templateUrl: 'partials/tour.html',
+        controller: 'tourCtrl'
+      });
+});
+
+twrApp.controller('homeCtrl', function($scope){});
+twrApp.controller('aboutCtrl', function($scope){});
+twrApp.controller('contactCtrl', function($scope){});
+twrApp.controller('mediaCtrl', function($scope){});
+twrApp.controller('tourCtrl', function($scope){});
---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+		jQuery(function($) {
+		$('.slider').sss();
+		});
+
+})

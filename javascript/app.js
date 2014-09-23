691f170a3a9ac67bbe9fe8c36e3d8060cf748af3
timbits07 <tim.storey@yahoo.com>
2014-09-23 15:21:51 -0700

    header redesign, twitter reload fixed

--- javascript/app.js
+++ javascript/app.js
@@ -4,8 +4,12 @@ twrApp.config(function($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'partials/home.html',
-        controller: 'homeCtrl'
+        controller: 'siteCtrl'
     	})
+      .when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'homeCtrl'
+      })
       .when('/about', {
         templateUrl: 'partials/about.html',
         controller: 'aboutCtrl'
@@ -24,6 +28,16 @@ twrApp.config(function($routeProvider) {
       });
 });
 
+twrApp.controller('siteCtrl', function($scope, $http, $timeout){
+  $http({method: 'GET', url: 'http://twrblog.herokuapp.com/posts.json'}).
+  success(function(data, status, headers, config) {
+    $scope.post = data;
+  }).
+  error(function(data, status, headers, config) {
+    console.log("AJAX failed")
+  });
+})
+
 twrApp.controller('homeCtrl', function($scope, $http, $timeout){
   $http({method: 'GET', url: 'http://twrblog.herokuapp.com/posts.json'}).
   success(function(data, status, headers, config) {
@@ -32,16 +46,17 @@ twrApp.controller('homeCtrl', function($scope, $http, $timeout){
   error(function(data, status, headers, config) {
     console.log("AJAX failed")
   });
-  $http({method: 'GET', url: 'http://api.bandsintown.com/artists/the%20workday%20release/events.json?callback=?api_version=2.0&app_id=twr_site'}).
-  success(function(data, status, headers, config) {
-    $scope.shows = data;
-    console.log(data)
-  }).
-  error(function(data, status, headers, config) {
-    console.log("AJAX failed")
-  });
-  //This allows the twitter widget to reload when navigating back to hom through ng-routes
+  // $http({method: 'GET', url: 'http://api.bandsintown.com/artists/the%20workday%20release/events.json?callback=?api_version=2.0&app_id=twr_site'}).
+  // success(function(data, status, headers, config) {
+  //   $scope.shows = data;
+  //   console.log(data)
+  // }).
+  // error(function(data, status, headers, config) {
+  //   console.log("AJAX failed")
+  // });
+  //This allows the twitter widget to reload when navigating back to home through ng-routes
   $timeout = twttr.widgets.load();
+  
 });
 twrApp.controller('aboutCtrl', function($scope){});
 twrApp.controller('contactCtrl', function($scope){});

7723dacd420f28375d7899faf5cb3ab932850239
timbits07 <tim.storey@yahoo.com>
2015-08-20 20:58:31 -0700

    responsiveness complete

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-	
-	jQuery(function($) {
-		$('.slider').sss();
-	});
 
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
